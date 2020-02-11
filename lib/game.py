909bdc1efa9798512a473c482e36df5be69027d9
Jason Brewer <jbrewer@pdx.edu>
2020-02-10 16:32:42 -0800

    displays ending score. Moved code from main.py to game class.

--- lib/game.py
+++ lib/game.py
@@ -1,5 +1,7 @@
-from random import randrange
+import sys, pygame
 import lib.constants as constant
+from random import randrange
+from time import time
 from lib.tetrino import Tetrino
 from lib.game_board import GameBoard
 from lib.translatable import Translatable
@@ -46,12 +48,8 @@ class Game(Translatable):
         # and y offsets. For instance, a square is flush in the upper left 
         # quadrant, so the x offset can be between 0 and 8.
         min_x, max_x, min_y, _ = self.find_minimum_coords(block_locations)
-        # print(f'Min x: {min_x}')
-        # print(f'Max x: {max_x}')
         x_offset = randrange(10 - (max_x - min_x)) - min_x
         y_offset = 0 - min_y
-        # print(f'X offset: {x_offset}')
-        # print(f'Y offset:{y_offset}')
         return [x_offset, y_offset]
 
     def find_minimum_coords(self, block_locations):
@@ -86,3 +84,80 @@ class Game(Translatable):
 
     def check_game_over(self):
         return self.game_board.check_game_over()
+
+    def run(self):
+        new_tetrino = True
+        interval = .25
+        prev_time = time()
+        white = (255, 255, 255)     
+        green = (0, 255, 0) 
+        
+        pygame.init()
+        # https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
+        font = pygame.font.Font('freesansbold.ttf', self.block_size)
+
+        text = font.render(f'Total score is {self.score}', True, white)  
+        textRect = text.get_rect()
+  
+        # set the center of the rectangular object. 
+        textRect.center = (self.screen_size[0] // 2 , self.screen_size[1] // 2) 
+        #https://stackoverflow.com/questions/43503995/pygame-key-get-pressed-
+        # how-to-add-interval
+        screen = pygame.display.set_mode(self.screen_size)
+        pygame.display.set_caption('Pygame Tetris')
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                    sys.exit()
+
+            if new_tetrino == True:
+                end = self.check_game_over()
+                if end:
+                    while True:
+                        screen.blit(text, textRect)
+                        for event in pygame.event.get() : 
+                            if event.type == pygame.QUIT : 
+                                pygame.quit() 
+                                sys.exit()
+                        pygame.display.update()
+                curr = self.create_random_tetrino()
+                new_tetrino = False
+                self.update_board()
+
+            pressed = pygame.key.get_pressed()
+            left_move, right_move, down_move, rotate_move \
+                = self.game_board.check_movement(curr)
+
+            if down_move == False:
+                # We're at the bottom
+                new_tetrino = True
+                self.calculate_score()
+            if left_move == True:
+                if pressed[pygame.K_LEFT] and time() > prev_time + interval:
+                    self.move_tetrino(curr, -1, 0)
+                    prev_time = time()
+            if right_move == True:
+                if pressed[pygame.K_RIGHT] and time() > prev_time + interval:
+                    self.move_tetrino(curr, +1, 0)
+                    prev_time = time()
+            if rotate_move == True:
+                if pressed[pygame.K_UP] and time() > prev_time + interval:
+                    self.rotate(curr)
+                    prev_time = time()
+            if down_move == True:    
+                if pressed[pygame.K_DOWN] and time() > prev_time + interval or \
+                    time() > prev_time + interval:
+                    self.move_tetrino(curr, 0, +1)
+                    prev_time = time()
+
+            screen.fill(self.background_color)
+            self.update_board()
+
+            for _, tetrino in self.tetrino_set.items():
+                img = pygame.image.load(tetrino.image)
+                for location in tetrino.locations:
+                    screen.blit(img, (self.block_size * location[constant.X], 
+                            self.block_size * location[constant.Y]))
+
+            pygame.display.flip()
--- main.py
+++ main.py
@@ -17,75 +17,13 @@ if inp == 'large':
     size = constant.LARGE
 if inp == 'small':
     size = constant.SMALL
-else:
-    print('Game size not recognized. Please consult README.')
-    sys.exit()
 
 game = Game(size)
-new_tetrino = True
-interval = .25
-prev_time = time()
+game.run()
 
-
-#https://stackoverflow.com/questions/43503995/pygame-key-get-pressed-how-to-add-interval
-# Need to convert the game grid into pygame
-screen = pygame.display.set_mode(game.screen_size)
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    if new_tetrino == True:
-        end = game.check_game_over()
-        if end:
-            print(f'Game over! Total score is {game.score}')
-            sys.exit()
-        curr = game.create_random_tetrino()
-        new_tetrino = False
-        game.update_board()
-        # game.display_board()
-        print(f'Score is {game.score}')
-
-    pressed = pygame.key.get_pressed()
-    left_move, right_move, down_move, rotate_move \
-         = game.game_board.check_movement(curr)
-
-    if down_move == False:
-        # We're at the bottom
-        new_tetrino = True
-        game.calculate_score()
-
-    if left_move == True:
-        if pressed[pygame.K_LEFT] and time() > prev_time + interval:
-            game.move_tetrino(curr, -1, 0)
-            prev_time = time()
-    if right_move == True:
-        if pressed[pygame.K_RIGHT] and time() > prev_time + interval:
-            game.move_tetrino(curr, +1, 0)
-            prev_time = time()
-    if rotate_move == True:
-        if pressed[pygame.K_UP] and time() > prev_time + interval:
-            # print('pressed up')
-            # print(curr.locations)
-            game.rotate(curr)
-            # print(curr.locations)
-            prev_time = time()
-
-
-    if down_move == True:    
-        if pressed[pygame.K_DOWN] and time() > prev_time + interval or \
-            time() > prev_time + interval:
-            game.move_tetrino(curr, 0, +1)
-            prev_time = time()
-
-    screen.fill(game.background_color)
-    game.update_board()
-
-    
-    for _, tetrino in game.tetrino_set.items():
-        img = pygame.image.load(tetrino.image)
-        for location in tetrino.locations:
-            screen.blit(img, (game.block_size * location[constant.X], 
-                    game.block_size * location[constant.Y]))
-
-
-    pygame.display.flip()
+# try:
+#     game = Game(size)
+#     game.run()
+# except NameError:
+#     print('Size not input correctly. Please consult README')
+#     sys.exit()
